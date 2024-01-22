ce97c5585eb087d20d59295c686bb335b4d5f41e
Farid Karimli <karimli.farid@icloud.com>
2024-01-21 20:49:35 -0500

    Timer and numbering

--- gui.py
+++ gui.py
@@ -12,16 +12,18 @@ TODAY_DATE = datetime.datetime.today().strftime("%d-%m-%Y-%H-%M")
 print(TODAY_DATE)
 
 movement_log = open(f"movement_log_{TODAY_DATE}" + ".txt", "w")
+
+# LOGS
  
 stats_log = open(f"stats_log_{TODAY_DATE}.csv", "w")
 stats_log.write("Time, Click X, Click Y, Target X, Target Y, Target Number, Distance to TargetX, Distance to TargetY, Select X, Select Y, From X, From Y\n")
 
-stats2_log = open(f"stats2_log_{TODAY_DATE}.txt", "w")
+secondary_stats_log = open(f"secondary_stats_log_{TODAY_DATE}.txt", "w")
 
 runs_log = open(f"runs_log_{TODAY_DATE}.csv", "w")
-runs_log.write("Run, Throughput, Ballistic Time, Select Time, Number of Targets\n")
+runs_log.write("Run, Throughput, Ballistic Time, Select Time, Number of TARGETS\n")
 
-# Create a dictionary of gestures and their corresponding numbers
+# Create a dictionary of gestures and their corresponding numbers - UNUSED
 throughputs = {"Gesture 1": [],  "Gesture 2": [], "Gesture 3": []}
 ballistics = {"Gesture 1": [], "Gesture 2": [], "Gesture 3": []}
 selects = {"Gesture 1": [],  "Gesture 2": [], "Gesture 3": []}
@@ -35,23 +37,18 @@ selection_coordinates = []
 
 window = tk.Tk()
 window.title("Testing GUI")
-'''window.attributes('-fullscreen',True)
-width = window.winfo_width()
-height = window.winfo_height()'''
 
 width = 1500
-height = 800
+height = 900
 
-
-
-targets = 0
-block = 0
-trial = 0
-
+# UNUSED
+TARGETS = 0
+BLOCK = 0
+TRIAL = 0
 TRIALS = 5
 BLOCKS = 3
 
-buttons_d = []
+buttons_dict = {}
 
 def distance(x1, y1, x2, y2):
     return ((x1-x2)**2 + (y1-y2)**2)**0.5
@@ -60,7 +57,7 @@ def pause(event):
     print("Paused")
     timer.pause()
     timer2.pause()
-    stats2_log.write(f"Pause\n")
+    secondary_stats_log.write(f"Pause\n")
     stats_log.write(f"Pause\n")
 
     pause_button.config(text="Paused!")
@@ -74,25 +71,23 @@ def continue_timer(event):
 
 def change_gesture():
     runs_log.write(f"Change Gesture\n")
-    stats2_log.write(f"Change Gesture\n")
+    secondary_stats_log.write(f"Change Gesture\n")
     stats_log.write(f"Change Gesture\n")
 
     change_gesture_button.config(text="Changed!")
 
-
-
 def remove_button(event, button_id):
-    global targets
+    global TARGETS
 
     print("This is the button id: ", button_id)
-    '''if buttons_d[targets] != event.widget: # Can't click on this button
+    '''if buttons_dict[TARGETS] != event.widget: # Can't click on this button
         return'''
     
     # Write click to movement log
     x,y = window.winfo_pointerx(), window.winfo_pointery()
     movement_log.write(
-        f"Click at ({x}, {y}) target {targets}. Time: {timer.get_elapsed()}\n")
-
+        f"Click at ({x}, {y}) target {TARGETS}. Time: {timer.get_elapsed()}\n")
+    
     checkpoint = timer.stop()
     timer.start()
     print("checkpoint:", checkpoint)
@@ -102,8 +97,7 @@ def remove_button(event, button_id):
     #print(click_checkpoint)
     fits.time_to_select += [click_checkpoint]
     
-    
-    stats2_log.write(f"Select: {button_id}, {x}, {y}, {click_checkpoint}, {fits.f}\n")
+    secondary_stats_log.write(f"Select: {button_id}, {x}, {y}, {click_checkpoint}, {fits.f}\n")
 
     fits.to = event.widget.winfo_rootx(), event.widget.winfo_rooty()
     fits.select = x,y
@@ -112,21 +106,65 @@ def remove_button(event, button_id):
     stats_log.write(  # Time, Click X, Click Y, Target X, Target Y, Target Number, Distance to TargetX, Distance to TargetY, Select X, Select Y, From X, From Y, Select time, Ballistic Time
         f"{checkpoint}, {x}, {y}, {event.widget.winfo_rootx()}, {event.widget.winfo_rooty()}, {button_id}, {fits.to[0]}, {fits.to[1]}, {fits.select[0]}, {fits.select[1]}, {fits.f[0]}, {fits.f[1]}\n")
 
+    if button_id != 0:
+        fits.update()
+        fits.times += [checkpoint]
 
-    fits.update()
     fits.f = x,y
-    fits.times += [checkpoint]
+    
+    TARGETS += 1
 
-    targets += 1
-
-    if targets == 10:
-        global trial
-        trial += 1
+    if TARGETS == 10:
+        global TRIAL
+        TRIAL += 1
         event.widget.place_forget()
         reset(None, True)
 
+    event.widget.place_forget()
+
+def remove_button2(event, button_id):
+    # do the same as remove_button, but for the first button - start the timer. For the last button, stop the timer. For all other buttons, end timer, do fits law, and start timer again
+    global TARGETS
+
+    print("This is the button id: ", button_id)
+    '''if buttons_dict[TARGETS] != event.widget: # Can't click on this button
+        return'''
+    
+    # Write click to movement log
+    x,y = window.winfo_pointerx(), window.winfo_pointery()
+    movement_log.write(
+        f"Click at ({x}, {y}) target {TARGETS}. Time: {timer.get_elapsed()}\n")
+    
+    checkpoint = timer.stop()
+    timer.start()
+    print("checkpoint:", checkpoint)
+    
+    click_checkpoint = timer2.get_elapsed()
+    timer2.start()
+    #print(click_checkpoint)
+    fits.time_to_select += [click_checkpoint]
+
+    secondary_stats_log.write(f"Select: {button_id}, {x}, {y}, {click_checkpoint}, {fits.f}\n")
+
+    fits.to = event.widget.winfo_rootx(), event.widget.winfo_rooty()
+    fits.select = x,y
+
+    # write stats to stats log
+    
+    fits.update()
+    fits.f = x, y
+    fits.times += [checkpoint]
+
+    TARGETS += 1
+
+    if TARGETS == 10:
+        global TRIAL
+        TRIAL += 1
+        event.widget.place_forget()
+        reset(None, True)
 
     event.widget.place_forget()
+
 
 def place_directional_targets():
 
@@ -149,45 +187,50 @@ def place_directional_targets():
         newbutton.bind("<1>", remove_button)
         newbutton.place(x=x, y=y, anchor="center")
 
-
-    
 def place_simple_targets():
     x = 0
     for j in [2,4]:
         for i in range(1,10,2):
             target = tk.Button(window, text=f"Target {x+1}", width=8, height=2, highlightbackground='gray', bg="gray", fg="black", font=("Arial", 15))
-            buttons_d[x] = target
+            buttons_dict[x] = target
             target.place(x=100*i, y=100*j, anchor="center")
             target.bind("<1>", remove_button)
             target.bind("<Enter>", mouseover)
             x+=1
 
 def place_circle_targets():
-    # Place 10 targets in a circle around the center of the screen
-    global buttons_d
+    # Place 10 TARGETS in a circle around the center of the screen
+    global buttons_dict
 
     for i in range(10):
+        if i == 0:
+            number = 1
+        elif i == 9:
+            number = 10
+        elif i == 5:
+            number = 2
+        else:
+            number = prev_number + 2
+        
+
         arc = i*36
         x = width//2+70 + 350*math.cos(math.radians(arc))
         y = height//2 + 350*math.sin(math.radians(arc))
 
-        target = tk.Button(window, text=f"Target {i+1}", width=12, height=4, highlightbackground='gray', bg="gray", fg="black", font=("Arial", 16))
-        buttons_d += [target]
+        target = tk.Button(window, text=f"{number}", width=3,  highlightbackground='black',
+                           bg="black", fg="white", font=("Arial", 40), padx=0, pady=0, activebackground="gray", activeforeground="red", relief="raised")
+        buttons_dict[number] = target
         target.place(x=x, y=y, anchor="center")
-        target.bind("<1>", lambda event, id=i: remove_button(event, id))
-        target.bind("<Enter>", lambda event, id=i: mouseover(event, id))
+        target.bind("<1>", lambda event, id=number: remove_button(event, id))
+        target.bind("<Enter>", lambda event, id=number: mouseover(event, id))
 
-
+        prev_number = number
     # place a pause button in the top left of the screen
-    pause_button.place(x=100, y=50, anchor="center")
-
-    
-
+    #pause_button.place(x=100, y=50, anchor="center")
+    print(buttons_dict)
     # place a continue button next to the pause button
-    continue_button.place(x=200, y=50, anchor="center")
+    #continue_button.place(x=200, y=50, anchor="center")
 
-
-    
 def increase_size():
     width = button.winfo_width()
     height = button.winfo_height()
@@ -197,11 +240,11 @@ def decrease_size():
     button.config(width=button.winfo_width()-1, height=button.winfo_height()-1)
 
 def start_test():
-    global targets
-    targets = 0
+    global TARGETS
+    TARGETS = 0
 
-    timer.start()
-    timer2.start()
+    # timer.start()
+    # timer2.start()
 
     throughput_label.place_forget()
     ballistic_time_label.place_forget()
@@ -209,7 +252,6 @@ def start_test():
     start_button.place_forget()
     instructions.place_forget()
     change_gesture_button.place_forget()
-
 
     # Get cursor's current x and y coordinates
     x,y = window.winfo_pointerx(), window.winfo_pointery()
@@ -221,20 +263,20 @@ def start_test():
     place_circle_targets()
 
 def reset(event, remove_buttons=False):
-        global throughputs, ballistics, selects, gestures, trial, block, buttons_d, targets
+        global throughputs, ballistics, selects, gestures, TRIAL, BLOCK, buttons_dict, TARGETS
 
         if remove_buttons:
             print("removing buttons")
-            for button in buttons_d:
-                button.place_forget()
-            buttons_d.clear()
+            for button in buttons_dict:
+                buttons_dict[button].place_forget()
+            buttons_dict.clear()
             
-                
-        button.place_forget()
+        else:     
+            buttons_dict[button].place_forget()
         start_button.place(x=width//2, y=height//2+75, anchor="center")
 
-        """Show how many trials left in this block
-        instructions.config(text=f"Trial {trial} of {TRIALS} for this gesture")
+        """Show how many TRIALs left in this BLOCK
+        instructions.config(text=f"TRIAL {TRIAL} of {TRIALS} for this gesture")
         instructions.place(x=width//2, y=height//2-75, anchor="center")"""
 
     
@@ -242,18 +284,18 @@ def reset(event, remove_buttons=False):
         throughput = fits.calculate_modified_law(timer.stop())
         timer.start()
 
-        current_gesture = "Gesture " + str(block+1)
+        current_gesture = "Gesture " + str(BLOCK+1)
         throughputs[current_gesture] += [throughput]
         ballistics[current_gesture] += [stats[0]]
         selects[current_gesture] += [stats[1]]
 
         # Write to runs log
-        runs_log.write(f"{trial+1}, {throughput}, {stats[0]}, {stats[1]}, {targets}\n")
+        runs_log.write(f"{TRIAL+1}, {throughput}, {stats[0]}, {stats[1]}, {TARGETS}\n")
 
         
-        ''' if trial == TRIALS: 
-                trial = 0
-                block += 1
+        ''' if TRIAL == TRIALS: 
+                TRIAL = 0
+                BLOCK += 1
         '''
 
         average_throughput = np.mean(throughputs[current_gesture])
@@ -276,7 +318,7 @@ def reset(event, remove_buttons=False):
 
             
 
-        """if block == BLOCKS: # Finished all blocks
+        """if BLOCK == BLOCKS: # Finished all BLOCKs
             finished_label = tk.Label(window, text="Test Complete", font=("Helvetica", 18))
             finished_label.place(x=width//2, y=height//2-150, anchor="center")
 
@@ -286,13 +328,11 @@ def reset(event, remove_buttons=False):
     
             with results_file as f:
                 f.write("Gesture, Throughput, Ballistic Time, Select Time\n")
-                for block in range(BLOCKS):
-                    gesture_name = "Gesture " + str(block+1)
+                for BLOCK in range(BLOCKS):
+                    gesture_name = "Gesture " + str(BLOCK+1)
                     f.write(f"{gesture_name}, {round(np.mean(throughputs[gesture_name]),2)}, {round(np.mean(ballistics[gesture_name]),2)}, {round(np.mean(selects[gesture_name]),2)}\n")
                   """
                   
-                  
-
 button = tk.Button(window, text="Target", width=8, height=2, highlightbackground='#3E4149', fg="white", font=("Arial", 15))
 
 start_button = tk.Button(window, text="Start", width=10, height=2, highlightbackground='red', bg='red', fg="white", font=("Arial", 20),command=start_test)
@@ -302,8 +342,7 @@ pause_button = tk.Button(window, text="Pause", width=8, height=2, highlightbackg
 pause_button.bind("<1>", pause)
 pause_button.bind("p", pause)
 
-
-continue_button = tk.Button(window, text="Continue", width=6, height=2, highlightbackground='red', bg='green', fg="white", font=("Arial", 15))
+continue_button = tk.Button(window, text="Continue", width=8, height=2, highlightbackground='red', bg='green', fg="white", font=("Arial", 15))
 continue_button.bind("<1>", continue_timer)
 continue_button.bind('c', continue_timer)
 
@@ -323,20 +362,16 @@ reset_button = tk.Button(window, text="Reset", width=8, height=2, highlightbackg
 reset_button.place(x=width-20, y=0, anchor="ne")
 
 def mouseover(event, button_id):
-    if event.widget == buttons_d[targets]:
-        enter_checkpoint = timer2.stop()
-        timer2.start()
-        fits.ballistic_times += [enter_checkpoint]
-        # get x and y coordinates of event
-        x,y = event.x, event.y
-        # write to stats2 log the button that was clicked and the x and y coordinates of the mouse and the ballistics time
-        stats2_log.write(f"Ballistic: {button_id}, {x}, {y}, {enter_checkpoint}\n")
-        
-
-
+    enter_checkpoint = timer2.stop()
+    timer2.start()
+    fits.ballistic_times += [enter_checkpoint]
+    # get x and y coordinates of event
+    x,y = event.x, event.y
+    # write to stats2 log the button that was clicked and the x and y coordinates of the mouse and the ballistics time
+    secondary_stats_log.write(f"Ballistic: {button_id}, {x}, {y}, {enter_checkpoint}\n")
+    
 def key(event):
     window.event_generate('<Motion>', warp=True, x=width//2, y=height//2)
-
 
 def motion(event):
     x, y = event.x, event.y
@@ -347,9 +382,6 @@ window.bind('<Motion>', motion)
 window.bind('<space>', key)
 # Bind the reset funtion to clicking q
 button.bind("<Enter>", mouseover)
-
-'''window.bind('<P>', lambda event: pause(event))
-window.bind('<C>', lambda event: continue_timer(event))'''
 
 window.geometry(f'{width}x{height}')
 window.mainloop()
